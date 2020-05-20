994f0e1836e8ab2cffeb1be250b1ba65efc89229
kimlai <hello@kimlaitrinh.me>
2020-05-20 15:17:03 +0200

    Add loading state
    Also, do not hide the form when a result is displayed. It's less "wow"
    but more convenient.

--- mort-ou-pas-mort.js
+++ mort-ou-pas-mort.js
@@ -1,8 +1,16 @@
-form = document.getElementById("form");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const error = document.getElementById("error");
+const result = document.getElementById("result");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  value = document.getElementById("input").value;
+  const value = input.value;
+  button.classList.add("loading");
+  button.disabled = true;
+  result.innerHTML = "";
+  result.classList.remove("success");
   fetch(
     "https://fr.wikipedia.org/w/api.php?action=query&list=prefixsearch&utf8=&format=json&origin=*&pssearch=" +
       encodeURIComponent(value)
@@ -11,7 +19,9 @@ form.addEventListener("submit", e => {
     .then(response => response.query.prefixsearch)
     .then(results => {
       if (results.length === 0) {
-        document.getElementById("result").innerHTML = "Aucune idée !";
+        button.disabled = false;
+        button.classList.remove("loading");
+        result.innerHTML = "Aucune idée !";
       } else {
         const result = results[0];
         fetch(
@@ -21,6 +31,8 @@ form.addEventListener("submit", e => {
         )
           .then(body => body.json())
           .then(response => {
+            button.classList.remove("loading");
+            button.disabled = false;
             const parser = new DOMParser();
             const doc = parser.parseFromString(
               response.parse.text,
@@ -42,7 +54,6 @@ form.addEventListener("submit", e => {
               result = "Mort·e le " + formatDate(dday.getAttribute("datetime"));
             }
             document.getElementById("result").innerHTML = result;
-            form.classList.add("visually-hidden");
           });
       }
     });
